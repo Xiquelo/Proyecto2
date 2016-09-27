976ab61ee54cefbfe8427a446098b096c5b660ce
Ivanov <xiquelo@IvanovXA0.local>
2016-09-26 23:08:31 -0500

    2 líneas +

--- Proyecto2.py
+++ Proyecto2.py
@@ -1,4 +1,6 @@
-
+def Fact(rec=1):
+	Num = input("Ingresa el numero a calcular: ")
+	if Num 
 
 print "Bienvenido a tu calculadora de Factoriales"
-print "Ingresa el numeero a calcular"
+print "Ingresa el numero a calcular"
